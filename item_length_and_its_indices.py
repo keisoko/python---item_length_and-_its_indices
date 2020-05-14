ad7462fa8046d20a7cbf45c5be28fc7ee65da8c4
keisoko <keisoko@gmail.com>
2020-05-14 01:31:30 -0400

    Remove extra logger.info() statements

--- item_length_and_its_indices.py
+++ item_length_and_its_indices.py
@@ -9,7 +9,6 @@ def item_and_its_indices(item):
         else:
             logger.info(f"index[{index}] = {value}")
 
-logger.info(" ")
 
 """Logs length of the given item"""
 def item_length(item):
@@ -27,12 +26,10 @@ def item_length(item):
 my_list = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765]
 
 operate(item_and_its_indices, my_list)
-logger.info(" ")
 operate(item_length, my_list)
-logger.info(" ")
+
 
 string = "The Master and Margarita"
 
 operate(item_and_its_indices, string)
-logger.info(" ")
 operate(item_length, string)
