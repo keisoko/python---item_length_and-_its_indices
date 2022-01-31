8de446d6d504ffc0580a3f8ac0c253df18493e6e
Dmitriy Goldner <keisoko@gmail.com>
2022-01-31 02:02:49 -0500

    Change name of the parameter in the second function

--- my_python_modules.py
+++ my_python_modules.py
@@ -4,7 +4,7 @@ def flat_list_of_lists_sorted(nested_list):
     return sorted(list(nested_list | traverse))
 
 
-def flat_list_of_lists_unsorted(my_list):
+def flat_list_of_lists_unsorted(nested_list):
     return list(nested_list | traverse)
 
 
