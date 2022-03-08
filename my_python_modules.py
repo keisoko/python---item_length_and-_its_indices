329638fd662cf1b3c49b99941900d6833c8f3c57
Dmitriy Goldner <keisoko@gmail.com>
2022-03-07 19:24:38 -0500

    Remove functions unrelated to the project

--- my_python_modules.py
+++ my_python_modules.py
@@ -1,12 +1,2 @@
-from pipe import traverse
-
-def flat_list_of_lists_sorted(nested_list):
-    return sorted(list(nested_list | traverse))
-
-
-def flat_list_of_lists_unsorted(nested_list):
-    return list(nested_list | traverse)
-
-
 def operate(func, item):
     return func(item)
