adc6393819792df8c0db3e66515b0dc321829770
Dmitriy Goldner <keisoko@gmail.com>
2022-09-09 18:25:34 -0400

    Add pretty_print_item function and related imports

--- my_python_modules.py
+++ my_python_modules.py
@@ -2,6 +2,8 @@
 
 import random
 import string
+from pprint import pformat
+from typing import Any, Iterable
 
 
 def operate(func, item):
@@ -12,3 +14,11 @@ def operate(func, item):
 def generate_id(length: int):
     """Helper function to generate id."""
     return "".join(random.choices(string.hexdigits.upper(), k=length))
+
+
+def pretty_print_item(item_to_pformat: Any, char_to_remove: Iterable[str]) -> str:
+    """Returns a string pretty formatted with pformat"""
+    formatted_item = pformat(item_to_pformat, underscore_numbers=True)
+    for char in char_to_remove:
+        formatted_item = formatted_item.replace(char, "")
+    return formatted_item
